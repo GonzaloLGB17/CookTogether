5dfc59fbf7c742e064d6f5d5066e3b2b80a0787a
Gonzalo <glopezg31@educastillalamancha.es>
2024-06-14 18:05:49 +0200

    connection

--- app/src/main/java/database/ConexionBBDD.java
+++ app/src/main/java/database/ConexionBBDD.java
@@ -13,7 +13,6 @@ public class ConexionBBDD {
     private static final String DRIVER = "org.postgresql.Driver";
     private static final String URL = "jdbc:postgresql://10.0.2.2:5432/cooktogether";
     private static final String USUARIO = "postgres";
-    String hola = "";
     private static final String PASSWORD = "admin";
 
     //Creamos nuestra funcion para conectarnos a Postgresql
