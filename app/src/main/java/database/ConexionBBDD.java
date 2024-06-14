969bb6939f742516b7163358e89c0b4a6b39a0ea
Gonzalo <glopezg31@educastillalamancha.es>
2024-06-14 18:00:41 +0200

    conbbdd

--- app/src/main/java/database/ConexionBBDD.java
+++ app/src/main/java/database/ConexionBBDD.java
@@ -11,11 +11,9 @@ public class ConexionBBDD {
 
     private Connection conexion = null;
     private static final String DRIVER = "org.postgresql.Driver";
-    private static final String URL = "jdbc:postgresql://192.168.1.141:5432/cooktogether";
-    private static final String URLL = "jdbc:postgresql://cooktogether.cnugm68gcria.eu-north-1.rds.amazonaws.com/cooktogether";
+    private static final String URL = "jdbc:postgresql://10.0.2.2:5432/cooktogether";
     private static final String USUARIO = "postgres";
     private static final String PASSWORD = "admin";
-    private static final String CONTR = "Casaergroup132456";
 
     //Creamos nuestra funcion para conectarnos a Postgresql
     public Connection conectarDB(){
@@ -24,7 +22,6 @@ public class ConexionBBDD {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
             Class.forName(DRIVER);
-            //conexion = DriverManager.getConnection(URLL, USUARIO, CONTR);
             conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
         }catch (SQLException e){
             System.out.println("Error en la conexión a BBDD");
